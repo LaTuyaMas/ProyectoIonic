2aa38865abebf742deb6eeb4f83bb1719a088d39
miguelcanoroman <miguel.cano.roman@cieep.com>
2023-01-23 13:56:24 +0100

    waypoint

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,6 +16,18 @@ const routes: Routes = [
     path: 'serie',
     loadChildren: () => import('./pages/serie/serie.module').then( m => m.SeriePageModule)
   },
+  {
+    path: 'search',
+    loadChildren: () => import('./pages/search/search.module').then( m => m.SearchPageModule)
+  },
+  {
+    path: 'categories',
+    loadChildren: () => import('./pages/categories/categories.module').then( m => m.CategoriesPageModule)
+  },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)
+  },
 ];
 
 @NgModule({
